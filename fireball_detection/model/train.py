3cffe1b8771cd1748b4f278ea30481a8f0c2cea2
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-06-15 16:18:13 +0800

    tuned args

--- fireball_detection/model/train.py
+++ fireball_detection/model/train.py
@@ -1,14 +1,29 @@
 from ultralytics import YOLO
+import yaml
+from pathlib import Path
 
-model = YOLO("yolov8n.pt")
+
+# model = YOLO("yolov8n.pt")
+model = YOLO("runs/detect/train16/weights/last.pt")
 
 data = "../data/fireball_object_detection/data.yaml"
+
+
+kwargs = {}
+
+USE_TUNED_ARGS = False
+tuned_args_yaml = Path(Path(__file__).parents[1], "cfg", "tuned.yaml")
+if USE_TUNED_ARGS:
+    with open(tuned_args_yaml, 'r') as file:
+        kwargs = yaml.safe_load(file)
+
 
 model.train(
     data=data,
     epochs=100,
     imgsz=640,
-    pretrained=True
+    pretrained=True,
+    **kwargs
 )
 
 model.val(
