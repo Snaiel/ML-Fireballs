9b0af9a2bbb08286a28f78bc8e3cb35b84a39ab1
Snaiel <56014779+Snaiel@users.noreply.github.com>
2024-11-07 20:05:23 +0800

    rework model training

--- object_detection/model/train.py
+++ object_detection/model/train.py
@@ -1,31 +1,59 @@
-from ultralytics import YOLO
-import yaml
+import os
 from pathlib import Path
 
+import yaml
+from ultralytics import YOLO, settings
 
-model = YOLO("yolov8n.pt")
-# model = YOLO("runs/detect/train16/weights/last.pt")
+from object_detection.dataset import DATA_FOLDER
 
-data = Path(Path(__file__).parents[2], "data/object_detection/data.yaml")
-
-
-kwargs = {}
-
-with open(Path(Path(__file__).parents[1], "cfg", "split_tiles.yaml"), 'r') as file:
-    kwargs = yaml.safe_load(file)
+settings.update({"wandb": False})
 
 
-model.train(
-    data=data,
-    epochs=100,
-    imgsz=400,
-    pretrained=True,
-    **kwargs
-)
+def main():
+    data_files: list[Path] = []
+    for od_folder in [i for i in os.listdir(DATA_FOLDER) if i.startswith("object_detection")]:
+        for folder in os.listdir(Path(DATA_FOLDER, od_folder)):
+            data_yaml_file = Path(DATA_FOLDER, od_folder, folder, "data.yaml")
+            if data_yaml_file.exists():
+                data_files.append(data_yaml_file)
 
-model.val(
-    data=data,
-    imgsz=400,
-    plots=True,
-    split="test"
-)
+    if len(data_files) == 0:
+        print("No datasets found. Consider running")
+        print("  python3 -m object_detection.dataset.generate_dataset")
+        print("  python3 -m object_detection.dataset.create_kfold_dataset")
+        return
+
+    data_files = sorted(data_files)
+
+    print("Datasets found:")
+
+    for i in range(len(data_files)):
+        print(f"  [{i+1}] {data_files[i].relative_to(DATA_FOLDER)}")
+
+    while True:
+        try:
+            user_input = int(input("Enter which dataset to use: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+    data = data_files[user_input-1]
+
+    model = YOLO("yolov8n.pt")
+
+    kwargs = {}
+    with open(Path(Path(__file__).parents[1], "cfg", "split_tiles.yaml"), 'r') as file:
+        kwargs = yaml.safe_load(file)
+
+    model.train(
+        data=data,
+        epochs=100,
+        imgsz=416,
+        pretrained=True,
+        val=False if "/all/" in str(data) else True,
+        **kwargs
+    )
+
+
+if __name__ == "__main__":
+    main()
