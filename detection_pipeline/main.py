9bd5b8d58cc5ca953cdc09c5b1114f9d6f4fda25
Snaiel <56014779+Snaiel@users.noreply.github.com>
2025-01-07 18:37:05 +0800

    log detections in image sequence order

--- detection_pipeline/main.py
+++ detection_pipeline/main.py
@@ -11,7 +11,7 @@ from pathlib import Path
 import numpy as np
 import structlog
 from skimage import io
-from structlog.typing import WrappedLogger
+from structlog.typing import WrappedLogger, EventDict
 from tqdm import tqdm
 
 from detection_pipeline import MAX_TIME_DIFFERENCE, MIN_DIAGONAL_LENGTH
@@ -19,14 +19,14 @@ from detection_pipeline.image_differencing import difference_images
 from fireball_detection.detect import (detect_differenced_tiles,
                                        get_absolute_fireball_boxes,
                                        merge_bboxes)
-from object_detection.detectors import DetectorSingleton
+from object_detection.detectors import Detector, DetectorSingleton
 from object_detection.utils import diagonal_length
 
 
 VERSION = "1.0.0"
 
 
-def module_processor(logger: WrappedLogger, log_method: str, event_dict: dict):
+def module_processor(logger: WrappedLogger, log_method: str, event_dict: EventDict) -> EventDict:
     path = event_dict.pop("pathname")
     module_path = str(Path(path).relative_to(Path(__file__).parents[1]))
     module = module_path.replace("/", ".").replace(".py", "")
@@ -34,7 +34,7 @@ def module_processor(logger: WrappedLogger, log_method: str, event_dict: dict):
     return event_dict
 
 
-def reorder_event_dict(logger: WrappedLogger, log_method: str, event_dict: dict) -> dict:
+def reorder_event_dict(logger: WrappedLogger, log_method: str, event_dict: EventDict) -> EventDict:
     reordered_dict = {}
     reordered_dict["timestamp"] = event_dict.pop("timestamp")
     reordered_dict["process_name"] = event_dict.pop("process_name")
@@ -83,6 +83,7 @@ class Args:
 class WorkerProcessArgs:
     triples_queue: mp.Queue
     bar_queue: mp.Queue
+    detection_log_queue: mp.Queue
     folder_path: Path
     output_folder: Path
     model_path: Path
@@ -91,42 +92,51 @@ class WorkerProcessArgs:
 
 @dataclass
 class ImagesTriple:
+    index: int
     before: str | None
     current: str
     after: str | None
 
 
-class WorkerProcess(mp.Process):
+class DetectionWorkerProcess(mp.Process):
     
+    args: WorkerProcessArgs
+    detector: Detector
+    log_messages: list[str]
+
+
     def __init__(self, args: WorkerProcessArgs):
         super().__init__()
-        self.triples_queue = args.triples_queue
-        self.bar_queue = args.bar_queue
-        self.folder_path = args.folder_path
-        self.output_folder = args.output_folder
-        self.detector = args.detector
-        self.model_path = args.model_path
+        self.args = args
+        self.log_messages = []
 
 
     def run(self):
-        self.detector = DetectorSingleton.get_detector(self.detector, self.model_path)
+        self.detector = DetectorSingleton.get_detector(self.args.detector, self.args.model_path)
+
+        processors: list = structlog.get_config()["processors"]
+        processors.insert(len(processors) - 1, DetectionLoggingProcessor(self))
+
         while True:
-            triple = self.triples_queue.get()
+            triple: ImagesTriple = self.args.triples_queue.get()
             if triple is None:
                 break
             self.process_triple(triple)
-            self.bar_queue.put(1)
+            self.args.detection_log_queue.put((triple.index, "\n".join(self.log_messages)))
+            self.log_messages.clear()
+            self.args.bar_queue.put(1)
+        
+        processors.pop(len(processors) - 1)
 
 
     def process_triple(self, images: ImagesTriple) -> None:
-
         structlog.contextvars.bind_contextvars(image=images.current)
         
-        image_current = io.imread(Path(self.folder_path, images.current))
+        image_current = io.imread(Path(self.args.folder_path, images.current))
 
         if images.before and images.after:
-            image_before = io.imread(Path(self.folder_path, images.before))
-            image_after = io.imread(Path(self.folder_path, images.after))
+            image_before = io.imread(Path(self.args.folder_path, images.before))
+            image_after = io.imread(Path(self.args.folder_path, images.after))
 
             differenced_image_pair1 = difference_images(image_current, image_before)
             differenced_image_pair2 = difference_images(image_current, image_after)
@@ -139,7 +149,7 @@ class WorkerProcess(mp.Process):
         else:
             differenced_image = difference_images(
                 image_current,
-                io.imread(Path(self.folder_path, images.before)) if images.before else io.imread(Path(self.folder_path, images.after))
+                io.imread(Path(self.args.folder_path, images.before)) if images.before else io.imread(Path(self.args.folder_path, images.after))
             )
         
         fireball_name = images.current.split(".")[0]
@@ -164,13 +174,13 @@ class WorkerProcess(mp.Process):
         logger.info("detected_fireballs", detected_fireballs=detections)
 
         if detected_fireballs:
-            fireball_folder = Path(self.output_folder, fireball_name)
+            fireball_folder = Path(self.args.output_folder, fireball_name)
             os.mkdir(fireball_folder)
 
-            shutil.copy(Path(self.folder_path, images.current), fireball_folder)
+            shutil.copy(Path(self.args.folder_path, images.current), fireball_folder)
 
             io.imsave(
-                Path(fireball_folder, images.current + "_differenced.jpg"),
+                Path(fireball_folder, images.current.removesuffix("jpg") + "differenced.jpg"),
                 differenced_image,
                 check_contrast=False,
                 quality=100
@@ -187,7 +197,7 @@ class WorkerProcess(mp.Process):
                     quality=100                                                 
                 )
                 io.imsave(
-                    Path(fireball_folder, tile_name + "_differenced.jpg"),
+                    Path(fireball_folder, tile_name + ".differenced.jpg"),
                     differenced_image[y1:y2, x1:x2],
                     check_contrast=False,
                     quality=100
@@ -201,6 +211,48 @@ class WorkerProcess(mp.Process):
                 json_file.write(output_json)
             
         structlog.contextvars.unbind_contextvars("image")
+
+
+class DetectionLoggingProcessor:
+    
+    def __init__(self, worker_process: DetectionWorkerProcess):
+        self.worker_process = worker_process
+
+    
+    def __call__(self, logger: WrappedLogger, name: str, event_dict: EventDict):
+        self.worker_process.log_messages.append(json.dumps(event_dict, indent=4))
+        raise structlog.DropEvent
+
+
+class DetectionLoggerProcess(mp.Process):
+    
+    detection_messages: dict
+    current_index: int
+    max_index: int
+    logger: structlog.WriteLogger
+
+
+    def __init__(self, detection_log_queue: mp.Queue, max_index: int):
+        super().__init__()
+        self.detection_log_queue = detection_log_queue
+        self.detection_messages = dict()
+        self.current_index = 0
+        self.max_index = max_index
+        self.logger = structlog.get_config()["logger_factory"]()
+    
+
+    def run(self):
+        while self.current_index < self.max_index:
+            index, msg = self.detection_log_queue.get(True)
+
+            if index == self.current_index:
+                self.logger.msg(msg)
+                self.current_index += 1
+                while self.current_index in self.detection_messages:
+                    self.logger.msg(self.detection_messages.pop(self.current_index))
+                    self.current_index += 1
+            else:
+                self.detection_messages[index] = msg
 
 
 def update_bar(bar_queue: mp.Queue, total: int) -> None:
@@ -255,7 +307,9 @@ def main() -> None:
         time_seconds = dt.hour * 3600 + dt.minute * 60 + dt.second
         return time_seconds
 
+
     triples_queue = mp.Queue()
+    index = 0
 
     for i in range(len(images)):
         
@@ -272,30 +326,39 @@ def main() -> None:
             after = images[i+1]
 
         if not (before or after):
+            logger.info("image skipped due to lack of recent before or after images", image=current)
             continue
         
         triples_queue.put(
             ImagesTriple(
+                index,
                 before,
                 current,
                 after
             )
         )
 
+        index += 1
+
     bar_queue = mp.Queue()
     bar_process = mp.Process(target=update_bar, args=(bar_queue, triples_queue.qsize()), daemon=True)
     bar_process.start()
 
+    detection_log_queue = mp.Queue()
+    detection_logger = DetectionLoggerProcess(detection_log_queue, triples_queue.qsize())
+    detection_logger.start()
+
     process_args = WorkerProcessArgs(
         triples_queue,
         bar_queue,
+        detection_log_queue,
         folder_path,
         output_folder,
         model_path,
         args.detector
     )
 
-    processes = [WorkerProcess(process_args) for _ in range(args.processes)]
+    processes = [DetectionWorkerProcess(process_args) for _ in range(args.processes)]
 
     for _ in processes:
         triples_queue.put(None)
