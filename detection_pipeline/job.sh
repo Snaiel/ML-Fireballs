5cf6124fa7e3851e916ae6ec363d6ca6ada64929
Snaiel <56014779+Snaiel@users.noreply.github.com>
2025-01-07 22:46:31 +0800

    more setonix config stuff

--- detection_pipeline/job.sh
+++ detection_pipeline/job.sh
@@ -13,11 +13,25 @@ fi
 
 module unload python
 module load pytorch/2.2.0-rocm5.7.3
+
+module load py-matplotlib/3.8.1
+module load py-numpy/1.26.1
+module load py-pandas/2.1.2
+module load py-scikit-learn/1.3.2
+module load py-pip/23.1.2-py3.11.6
+
 bash
 
 cd $MYSOFTWARE/ML-Fireballs
 
-source .venv/bin/activate
+if [ ! -d ".venv" ]; then
+    echo ".venv does not exist. Creating virtual environment and installing dependencies..."
+    python3 -m venv .venv --system-site-packages
+    source .venv/bin/activate
+    pip install -r requirements.txt
+else
+    source .venv/bin/activate
+fi
 
 python3 -m detection_pipeline.main \
     --folder_path "$FOLDER_PATH" \
