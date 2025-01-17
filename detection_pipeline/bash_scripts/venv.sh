6dc09479dc4d88a9be24ebdd2140377f22cda097
Snaiel <56014779+Snaiel@users.noreply.github.com>
2025-01-17 23:46:01 +0800

    module load and enter subshell

--- detection_pipeline/bash_scripts/venv.sh
+++ detection_pipeline/bash_scripts/venv.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+module load pytorch/2.2.0-rocm5.7.3
+
+bash << 'EOF'
+
+cd $MYSOFTWARE/ML-Fireballs
 python3 -m venv .venv --system-site-packages
 source .venv/bin/activate
 mkdir -p $MYSCRATCH/tmp
@@ -6,3 +12,5 @@ export TMPDIR="$MYSCRATCH/tmp"
 mkdir -p "$TMPDIR"
 pip install --cache-dir="$TMPDIR" -r requirements.txt
 pip install --cache-dir="$TMPDIR" onnxruntime
+
+EOF
