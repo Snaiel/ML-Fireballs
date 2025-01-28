554fb246eef68cfe73ded36012823534f0a632b9
Snaiel <56014779+Snaiel@users.noreply.github.com>
2025-01-28 14:06:31 +0800

    specify before and after images

--- detection_pipeline/image_differencing/create_differenced_images.py
+++ detection_pipeline/image_differencing/create_differenced_images.py
@@ -1,5 +1,9 @@
+import argparse
+import json
 import os
+import shutil
 from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass
 from pathlib import Path
 
 import numpy as np
@@ -47,13 +51,41 @@ def process_image_chunk(args: list):
     )
 
 
+@dataclass
+class Args:
+    images_folder: str
+    output_folder: str | None
+    overwrite: bool
+
+
 def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Create differenced images from before, current, and after images",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--images_folder", type=str, required=True, help="Path to the folder containing before, current, and after images")
+    parser.add_argument("--output_folder", type=str, required=False, default=None,
+        help="The folder that will be created to contain differenced images. If None, will be under <images_folder>/differenced_images/")
+    parser.add_argument('--overwrite', action='store_true', default=False, help='Overwrite the output directory if it exists.')
+    
+    args = Args(**vars(parser.parse_args()))
+    print("\nargs:", json.dumps(vars(args), indent=4), "\n")
 
-    folder_2015_before_after = Path("data/2015_before_after")
-    folder_differenced_images = Path(folder_2015_before_after, "differenced_images")
+    folder_2015_before_after = Path(args.images_folder)
+    if args.output_folder:
+        folder_differenced_images = Path(args.output_folder)
+    else:
+        folder_differenced_images = Path(folder_2015_before_after, "differenced_images")
 
-    if not folder_differenced_images.exists():
-        os.mkdir(folder_differenced_images)
+    if folder_differenced_images.exists():
+        if args.overwrite:
+            print("removing existing folder...\n")
+            shutil.rmtree(folder_differenced_images)
+        else:
+            print(f"\"{folder_differenced_images}\" already exists. include --overwrite option to overwrite folders.")
+            return
+    else:
+        os.makedirs(folder_differenced_images, exist_ok=True)
 
     differenced_images = set(os.listdir(folder_differenced_images))
     images = [i for i in sorted(os.listdir(folder_2015_before_after)) if Path(folder_2015_before_after, i).is_file()]
