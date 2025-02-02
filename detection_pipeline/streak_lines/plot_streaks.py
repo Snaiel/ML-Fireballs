c990efd11836b103c4bdc9105ecc853e2120e862
Snaiel <56014779+Snaiel@users.noreply.github.com>
2025-02-02 15:28:28 +0800

    just one output folder plot_steaks

--- detection_pipeline/streak_lines/plot_streaks.py
+++ detection_pipeline/streak_lines/plot_streaks.py
@@ -20,70 +20,67 @@ def main():
         description="Plot a .differenced.jpg detection",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument("folder_path", type=str, help="Detection outputs folder which contains folders for each camera")
+    parser.add_argument("folder_path", type=str, help="Detection outputs folder containing subfolders for each image")
 
     args = Args(**vars(parser.parse_args()))
     print("\nargs:", json.dumps(vars(args), indent=4), "\n")
 
-    camera_folders = [i for i in sorted(os.listdir(args.folder_path)) if Path(args.folder_path, i).is_dir()]
+    folder_path = Path(args.folder_path)
+
+    subfolders = sorted([d for d in folder_path.iterdir() if d.is_dir()])
+
+    with open(Path(subfolders[0], subfolders[0].name + ".json")) as json_file:
+        json_data = json.load(json_file)
+
+    original_image = Path(subfolders[0], json_data["original_image"])
+
+    detections_images: list[Path] = []
+
+    for subfolder in subfolders:
+        with open(Path(subfolder, subfolder.name + ".json")) as json_file:
+            json_data = json.load(json_file)
+        for detection in map(lambda x: x["name"], json_data["detections"]):
+            detections_images.append(Path(subfolder, detection + ".differenced.jpg"))
+
+    original = ski.io.imread(original_image)
+
+    _, ax = plt.subplots(figsize=(10, 10))
+    ax.imshow(original, cmap="gray", aspect="equal")
+    ax.set_title(f"Detections on {folder_path.name}")
+
+    colors = ["blue", "yellow", "orange", "green", "red", "purple", "cyan"]
+
+    with Pool() as pool:
+        streak_lines = pool.map(create_streak_line, detections_images)
 
     print("Invalid lines:")
 
-    for camera in camera_folders:
+    for idx, streak in enumerate(streak_lines):
 
-        camera_folder = Path(args.folder_path, camera)
-
-        subfolders = [i for i in sorted(os.listdir(camera_folder)) if Path(camera_folder, i).is_dir()]
-
-        if not subfolders:
+        if not streak.is_valid:
+            print(detections_images[idx].name)
             continue
 
-        original_image = Path(camera_folder, subfolders[0], subfolders[0] + ".thumb.jpg")
+        start_point = streak.startpoint
+        end_point = streak.endpoint
 
-        detections_images: list[Path] = []
+        ax.plot(
+            [start_point[0], end_point[0]],
+            [start_point[1], end_point[1]],
+            linestyle="-",
+            color=colors[idx % len(colors)]
+        )
 
-        for subfolder in subfolders:
-            detections = [i for i in sorted(os.listdir(Path(camera_folder, subfolder))) if "differenced" in i and "thumb" not in i]
-            for detection in detections:
-                detections_images.append(Path(camera_folder, subfolder, detection))
+        ax.text(
+            streak.midpoint[0],
+            streak.midpoint[1],
+            streak.number,
+            color="white"
+        )
 
-        original = ski.io.imread(original_image)
-
-        _, ax = plt.subplots(figsize=(10, 10))
-        ax.imshow(original, cmap="gray", aspect="equal")
-        ax.set_title(f"Detections on {Path(camera_folder).name}")
-
-        colors = ["blue", "yellow", "orange", "green", "red", "purple", "cyan"]
-
-        with Pool() as pool:
-            streak_lines = pool.map(create_streak_line, detections_images)
-
-        for idx, streak in enumerate(streak_lines):
-
-            if not streak.is_valid:
-                print(detections_images[idx].name)
-                continue
-
-            start_point = streak.startpoint
-            end_point = streak.endpoint
-
-            ax.plot(
-                [start_point[0], end_point[0]],
-                [start_point[1], end_point[1]],
-                linestyle="-",
-                color=colors[idx % len(colors)]
-            )
-
-            ax.text(
-                streak.midpoint[0],
-                streak.midpoint[1],
-                streak.number,
-                color="white"
-            )
-
-        plt.axis("off")
-        plt.tight_layout()
-        plt.show()
+    plt.axis("off")
+    plt.tight_layout()
+    plt.show()
     
 
 if __name__ == "__main__":
